95f621115d093d9c5f92c9916fe5d6bf41d737b5
Raam Dev <raam@raamdev.com>
2013-03-15 18:19:42 -0400

    Clean up unused journal custom post type.

--- functions-raam.php
+++ functions-raam.php
@@ -22,7 +22,6 @@ function publish_theme_pings($comment, $args, $depth) {
  */
 add_action( 'init', 'create_post_type_thoughts' );
 add_action( 'init', 'create_post_type_journal' );
-add_action( 'init', 'create_post_type_journalk09r4tyf58u35' );
 
 function create_post_type_thoughts() {	
 	register_post_type('thoughts', array(	'label' => 'Thoughts','description' => '','public' => true,'show_ui' => true,'show_in_menu' => true,'capability_type' => 'post','hierarchical' => false,'rewrite' => array('slug' => ''),'query_var' => true,'has_archive' => true,'supports' => array('title','editor','trackbacks','comments','revisions','custom-fields',),'taxonomies' => array('post_tag',),'labels' => array (
@@ -59,26 +58,6 @@ function create_post_type_journal() {
 	  'not_found' => 'No Journal Entries Found',
 	  'not_found_in_trash' => 'No Journal Entries Found in Trash',
 	  'parent' => 'Parent Journal Entry',
-	),) );
-}
-
-/* Used to hack the post_type RSS feed and prevent public from viewing the Journal feed */
-function create_post_type_journalk09r4tyf58u35() {
-	register_post_type('journalk09r4tyf58u35', array(	'label' => 'Journal_k09r4tyf58u35','description' => '','public' => true,'show_ui' => true,'show_in_menu' => false,'capability_type' => 'post','hierarchical' => false,'rewrite' => array('slug' => ''),'query_var' => true,'has_archive' => true,'supports' => array('title','editor','trackbacks','comments','revisions','custom-fields',),'taxonomies' => array('post_tag',),'labels' => array (
-	  'name' => 'Journalk09r4tyf58u35',
-	  'singular_name' => 'Journal Entry_k09r4tyf58u35',
-	  'menu_name' => 'Journal_k09r4tyf58u35',
-	  'add_new' => 'Add Journal_k09r4tyf58u35',
-	  'add_new_item' => 'Add New Journal_k09r4tyf58u35',
-	  'edit' => 'Edit',
-	  'edit_item' => 'Edit Journal_k09r4tyf58u35',
-	  'new_item' => 'New Journal_k09r4tyf58u35',
-	  'view' => 'View Journal_k09r4tyf58u35',
-	  'view_item' => 'View Journal_k09r4tyf58u35',
-	  'search_items' => 'Search Journal_k09r4tyf58u35',
-	  'not_found' => 'No Journal Entries Found_k09r4tyf58u35',
-	  'not_found_in_trash' => 'No Journal_k09r4tyf58u35 Entries Found in Trash',
-	  'parent' => 'Parent Journal_k09r4tyf58u35 Entry',
 	),) );
 }
 
