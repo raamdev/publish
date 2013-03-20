705b917c9255941a36f43a469f3becb19d6c213f
Raam Dev <raam@raamdev.com>
2013-03-19 23:09:26 -0400

    Add Page Template that adds a Tag Cloud to the footer.

---
+++ page-essays.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Template Name: Footer Tag Cloud 
+ */
+
+get_header(); ?>
+
+		<div id="primary" class="content-area">
+			<div id="content" class="site-content" role="main">
+
+				<?php while ( have_posts() ) : the_post(); ?>
+
+					<?php get_template_part( 'content', 'page' ); ?>
+					
+					<?php if ( function_exists('wp_tag_cloud') ) : ?>
+						<h2>Most Commonly Used Tags</h2>
+						<?php wp_tag_cloud('smallest=10&largest=22'); ?>
+					<?php endif; ?>
+
+					<?php comments_template( '', true ); ?>
+
+				<?php endwhile; // end of the loop. ?>
+
+			</div><!-- #content .site-content -->
+		</div><!-- #primary .content-area -->
+
+<?php get_sidebar(); ?>
+<?php get_footer(); ?>
