00921fbc01029095ef79cc816fc3bafa6d902fb0
Raam Dev <raam@raamdev.com>
2013-03-25 21:33:19 -0400

    Move trackback style code to style.css and clean up trackbacks a bit

--- functions-raam.php
+++ functions-raam.php
@@ -10,7 +10,7 @@ function publish_theme_pings($comment, $args, $depth) {
      <div id="comment-<?php comment_ID(); ?>">     
 
 	<div class="commentbody">
-<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?> <span style="font-size: 12px; color: #AAAAAA;"> <?php printf(__('%1$s '), get_comment_date("Y-m-d"),  get_comment_time("H:i:s")) ?> <?php edit_comment_link(__('(Edit)'),'  ','') ?></span>
+<?php printf(__('<cite class="fn">%s</cite>'), get_comment_author_link()) ?> <span> <?php printf(__('%1$s '), get_comment_date("Y-m-d"),  get_comment_time("H:i:s")) ?> <?php edit_comment_link(__('(Edit)'),'  ','') ?></span>
      </div>
 
      </div>
