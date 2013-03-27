e4fe1738c89a86afeb59a8c2ab80104d0bd2ef23
Raam Dev <raam@raamdev.com>
2013-03-27 15:29:03 -0400

    Add an ID to the pings list so it can be linked to directly.

--- comments.php
+++ comments.php
@@ -88,7 +88,7 @@ if ( post_password_required() )
 	<?php if ( have_comments() ) : ?>
 	<?php if ( count($wp_query->comments_by_type['pings'])) { ?>
 	<br />
-	<h3 class="comments-title"><?php// echo count($wp_query->comments_by_type['pings']); ?>Readers who shared <em><?php the_title(); ?></em></h3>
+	<h3 id="pings" class="comments-title"><?php// echo count($wp_query->comments_by_type['pings']); ?>Readers who shared <em><?php the_title(); ?></em></h3>
 	        <ul class="pinglist">
 	                <?php wp_list_comments('type=pings&callback=publish_theme_pings'); ?>
 	        </ul>
