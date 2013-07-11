5273a35acd7fef0e5521bf06532d4f87db55659f
Raam Dev <raam@raamdev.com>
2013-07-11 15:52:45 -0400

    * Manually insert Yoast Analytics code * Increase height of share via email iFrame

--- header.php
+++ header.php
@@ -16,6 +16,11 @@
 <link rel="profile" href="http://gmpg.org/xfn/11" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/inc/jquery-pageslide/jquery.pageslide.css">
+<?php
+if ( function_exists( 'yoast_analytics' ) ) {
+  yoast_analytics();
+}
+?>
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
