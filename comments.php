ea941eac826ae4e5076cd996ac7f6b82966a8d8c
Raam Dev <raam@raamdev.com>
2013-07-17 11:25:43 -0400

    * Add comment count next to 'Share a Comment' link * Reduce opacity of meta box until hovering to improve overall page focus

--- comments.php
+++ comments.php
@@ -26,12 +26,13 @@ if ( post_password_required() )
 	
 	<?php if ( have_comments() ) : ?>
 		<?php if ( !in_category('journal') || is_raamdev_journal_viewable() ) : ?>
-			<div class="leave-a-reply"><a href="#respond">Share a Comment ↓</a></div>
-			<!-- <h2 class="comments-title">
+			<div class="leave-a-reply">
 				<?php
-				printf( _n( 'One thought on &ldquo;%2$s&rdquo;', '%1$s thoughts on &ldquo;%2$s&rdquo;', get_comments_number(), 'publish' ),
-						number_format_i18n( get_comments_number() ), '<span>' . get_the_title() . '</span>' );
+				printf( _n( '1 Comment', '%1$s Comments', get_comments_number(), 'publish' ), number_format_i18n( get_comments_number() ) );
 				?>
+				| <a href="#respond">Share a Comment ↓</a></div>
+			<!-- <h2 class="comments-title">
+
 			</h2> -->
 
 			<?php if ( get_comment_pages_count() > 1 && get_option( 'page_comments' ) ) : // are there comments to navigate through ?>
